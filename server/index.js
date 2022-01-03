d87a11da84e6af885d6ead4f302ffdf773299db6
ElPoteito <58006516+ElPoteito@users.noreply.github.com>
2022-01-02 17:14:30 -0700

    Progreso en Interfaz de Usuario
    Se añadió la interfaz de usuario para los estados de cuenta del cliente.
    Se separaron las ventanas para registro y de estados de cuenta del archivo app.js

--- server/index.js
+++ server/index.js
@@ -21,11 +21,25 @@ app.post('/api/registro', (req, res) => {
     const apellidoMat = req.body.apellidoMat
     const telCel = req.body.telCel
 
-    const sqlInsert = "insert into Cuenta(nombre_p, apellido_p, apellido_m, numTelefonico, id_sucursal) values(? ,? ,? ,? ,1);"
+    const sqlInsert = "insert into Cuenta(nombre_p, apellido_p, apellido_m, numTelefonico, id_sucursal) values(? ,? ,? ,? ,2);"
     con.query(sqlInsert, [nombrePila, apellidoPat, apellidoMat, telCel], (err, result) => {
         console.log(err);
     });
 });
+
+app.post('/api/estado', (req, res) => {
+    const numeroTarjeta = req.body.numeroTarjeta
+    
+    const sqlRead = "select * from Transaccion where tarjetaOrigen = ? or tarjetaDestino = ?;"
+    con.query(sqlRead, [numeroTarjeta, numeroTarjeta], (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.send(result);
+        }
+    })
+})
 
 app.listen(3001, () => {
     console.log("running on port 3001");
