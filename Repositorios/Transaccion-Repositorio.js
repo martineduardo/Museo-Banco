370816af237c6a5b82a921cf09cb424e4d6582cd
Jorge29-bar <luisbarna1998@gmail.com>
2022-01-03 20:38:17 -0700

    Optimización de Transacciones
    Ya se pueden realizar transacciones, cuenta con validaciones de los saldos.

--- Repositorios/Transaccion-Repositorio.js
+++ Repositorios/Transaccion-Repositorio.js
@@ -5,10 +5,35 @@ class TransaccionRepositorio{
 
     async guardar(Datos){
             
-            //Validacion de Tarjetas
-            const callback = new Promise((resolve,reject) => {
+        //Validacion de Tarjetas
+        const callback = new Promise((resolve,reject) => {
+            con.query(
+                'Select numTarjeta From Tarjeta where numTarjeta = ?',
+                [Datos.tarjetaDestino],
+                function (error, results) {
+                    if(error) reject(error);
+                    resolve(results);
+                }
+            )
+        })
+
+        var responde = await callback.then(res => res).catch(err => {throw err});
+        var tarjeta = responde[0];
+
+        if(tarjeta != null){
+            const callbackOrigen = new Promise((resolve,reject) => {
                 con.query(
-                    'Select numTarjeta From Tarjeta where numTarjeta = ?',
+                    'Select saldo From Tarjeta where numTarjeta = ?',
+                    [Datos.tarjetaOrigen],
+                    function (error, results) {
+                        if(error) reject(error);
+                        resolve(results);
+                    }
+                )
+            })
+            const callbackDestino = new Promise((resolve,reject) => {
+                con.query(
+                    'Select saldo From Tarjeta where numTarjeta = ?',
                     [Datos.tarjetaDestino],
                     function (error, results) {
                         if(error) reject(error);
@@ -17,20 +42,66 @@ class TransaccionRepositorio{
                 )
             })
 
-            var responde = await callback.then(res => res).catch(err => {throw err});
-            var tarjeta = responde[0];
+            var responseOrigen = await callbackOrigen.then(res => res).catch(err => {throw err});
+            var saldoOrigen = responseOrigen[0].saldo;
 
-            if(tarjeta != null){
-                var query = con.query('INSERT INTO transaccion SET ?', Datos, function (error, results, fields) {
+            var responseDestino = await callbackDestino.then(res => res).catch(err => {throw err});
+            var saldoDestino = responseDestino[0].saldo;
+
+            var deposito = await this.jsonParser(Datos);
+
+            const date = new Date();
+            const mysqlDate = date.toISOString().split("T")[0];
+
+            if(saldoOrigen >= deposito){
+                
+                var saldoRestado = saldoOrigen - deposito;
+                var saldoTotal = saldoDestino + deposito;
+                
+                var query = con.query('INSERT INTO transaccion SET fecha = ?, ?, estado = ?',
+                [mysqlDate,
+                Datos,
+                1],
+                function (error, results, fields) {
                     if (error) throw error;
-                    // Neat!
-                    });
-                    console.log(query.sql);
-                    console.log("Transacción exitosa!");
+                });
+
+                var query2 = con.query('Update tarjeta Set saldo = ? Where numTarjeta = ?',
+                [saldoRestado,
+                Datos.tarjetaOrigen],
+                function (error, results, fields){
+                    if(error) throw error;
+                });
+
+                var query3 = con.query('Update tarjeta Set saldo = ? Where numTarjeta = ?',
+                [saldoTotal,
+                Datos.tarjetaDestino],
+                function (error, results, fields){
+                    if(error) throw error;
+                });
+                console.log("Transaccion exitosa!");
+                
             }
             else{
-                console.log("Cuenta inexistente!");
+                var query = con.query('INSERT INTO transaccion SET fecha = ?, ?, estado = ?',
+                [mysqlDate,
+                Datos,
+                0],
+                function (error, results, fields) {
+                    if (error) throw error;
+                });
+                console.log("Fondos insuficientes!");
             }
+        }
+        else{
+            console.log("Cuenta inexistente!");
+        }
+    }
+
+    jsonParser(doubleValue){
+        var double = JSON.stringify(doubleValue);
+        var objectValue = JSON.parse(double);
+        return objectValue['monto'];
     }
 
     async enviar(Datos){
